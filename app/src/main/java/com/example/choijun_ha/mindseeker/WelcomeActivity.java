cc2e0d93558f05feebd341aee4e909f6df70b54f
jha235 <jha235@sfu.ca>
2018-02-05 19:20:52 -0800

    picture

---
+++ app/src/main/java/com/example/choijun_ha/mindseeker/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.choijun_ha.mindseeker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+    }
+}
