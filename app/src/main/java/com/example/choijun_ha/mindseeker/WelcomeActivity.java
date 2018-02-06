6be2bdda8d5e12aa1bceca7d2a5a64f9707722dc
jha235 <jha235@sfu.ca>
2018-02-05 20:41:00 -0800

    picture

--- app/src/main/java/com/example/choijun_ha/mindseeker/WelcomeActivity.java
+++ app/src/main/java/com/example/choijun_ha/mindseeker/WelcomeActivity.java
@@ -1,15 +1,30 @@
 package com.example.choijun_ha.mindseeker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
+        Thread myThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                    Intent intent = new Intent(getApplicationContext(), StartActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
     }
 }
