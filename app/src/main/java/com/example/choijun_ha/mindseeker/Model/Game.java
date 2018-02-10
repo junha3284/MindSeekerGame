f0a6cea45eb41bc08c1eda8a4d4e3e83ad9c77b1
Junha Choi <junhac@sfu.ca>
2018-02-09 18:37:54 -0800

    Create ramdom-Generate Mine

--- app/src/main/java/com/example/choijun_ha/mindseeker/Model/Game.java
+++ app/src/main/java/com/example/choijun_ha/mindseeker/Model/Game.java
@@ -2,6 +2,9 @@ package com.example.choijun_ha.mindseeker.Model;
 
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by choijun-ha on 2018-02-06.
  */
@@ -9,17 +12,19 @@ import android.widget.Button;
 public class Game {
     private int COL_NUM=4;
     private int ROW_NUM=6;
-    private int MINE_NUM;
+    private int MINE_NUM=6;
+    private int attemptNum = 0;
+    private int coveredMineNum = MINE_NUM;
 
     BTN[][] btns;
 
-    static Game game;
+    private static Game game;
 
     private Game(){
-        btns = new BTN[ROW_NUM][COL_NUM];
+        btns = new BTN[this.ROW_NUM][this.COL_NUM];
     }
 
-    static Game createGame(){
+    public static Game createGame(){
         if(game == null)
             return new Game();
         else
@@ -35,13 +40,40 @@ public class Game {
     }
 
     public void setSize(int row, int col){
-        this.COL_NUM = col;
         this.ROW_NUM = row;
+        this.COL_NUM = col;
+        this.btns = new BTN[this.ROW_NUM][this.COL_NUM];
+        this.generateMines();
     }
 
     public void setMineNum(int mineNum){
         this.MINE_NUM = mineNum;
-        game = null;
+        this.generateMines();
+    }
+
+    private void generateMines(){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i=0; i < COL_NUM*ROW_NUM; i++)
+            list.add(i);
+        Collections.shuffle(list);
+        for(int i=0; i < MINE_NUM; i++){
+            int temp = list.get(i);
+            int row = temp/COL_NUM;
+            int col = temp%COL_NUM;
+            btns[row][col].implantMine();
+            increaseColHintNum(col);
+            increaseRowHintNum(row);
+            btns[row][col].decHintNum(); // because hint num increase twice with above two functions
+        }
+    }
+
+    private void increaseRowHintNum(int row){
+        for(int i=0; i < COL_NUM; i++)
+            btns[row][i].incHintNum();
+    }
+    private void increaseColHintNum(int col){
+        for(int i=0; i < ROW_NUM; i++)
+            btns[i][col].incHintNum();
     }
 
 }
