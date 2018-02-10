f58dc6f53f7bbfbc4ac23c87f36ea7f77a87ea1d
jha235 <jha235@sfu.ca>
2018-02-09 19:13:38 -0800

    Merge branch 'master' of https://csil-git1.cs.surrey.sfu.ca/cmpt276An3/MindSeeker into 7-implement-playactivity
    # Conflicts:
    #	app/src/main/java/com/example/choijun_ha/mindseeker/WelcomeActivity.java

--- app/src/main/java/com/example/choijun_ha/mindseeker/PlayActivity.java
+++ app/src/main/java/com/example/choijun_ha/mindseeker/PlayActivity.java
@@ -1,18 +1,77 @@
 package com.example.choijun_ha.mindseeker;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+import com.example.choijun_ha.mindseeker.Model.Game;
 
 public class PlayActivity extends AppCompatActivity {
 
+    private Game g;
+    int NUM_ROWS=4;
+    int NUM_COLS=6;
+   // private Game game;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        chooseTableSize();
+        populateButtons();
+        g=Game.createGame();
     }
-    
+
+    private void chooseTableSize() {
+        //TODO:change NUM_ROWS,NUM_COLS
+        NUM_ROWS=g.getRowNum();
+        NUM_COLS=g.getColNum();
+    }
+
+    private void populateButtons() {
+        TableLayout table=(TableLayout)findViewById(R.id.TableForButtons);
+        for(int row=0;row<NUM_ROWS;row++)
+        {
+            TableRow tableRow= new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1.0f
+
+            ));
+            table.addView(tableRow);
+            for(int col=0;col<NUM_COLS;col++)
+            {
+                final int FINAL_ROW=row;
+                final int FINAL_COL=col;
+                Button button=new Button(this);
+                button.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        1.0f
+                ));
+                //make text not clip on small buttons
+                button.setPadding(0,0,0,0);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        gridButtonClicked(FINAL_ROW,FINAL_COL);
+                    }
+                });
+                tableRow.addView(button);
+            }
+        }
+    }
+
+    private void gridButtonClicked(int x,int y) {
+        //TODO:decide what button will do+uncomment game above
+    }
+
     public static Intent makeIntent(Context context){
         Intent intent = new Intent (context,PlayActivity.class);
         return intent;
--- app/src/main/java/com/example/choijun_ha/mindseeker/configActivity.java
+++ app/src/main/java/com/example/choijun_ha/mindseeker/configActivity.java
@@ -1,4 +1,14 @@
-package com.example.choijun_ha.mindseeker;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import com.example.choijun_ha.mindseeker.Model.Game;
+import com.example.choijun_ha.mindseeker.R;ackage com.example.choijun_ha.mindseeker;
 
 import android.content.Context;
 import android.content.Intent;
@@ -9,16 +19,14 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-import com.example.choijun_ha.mindseeker.Model.Game;
-
 public class configActivity extends AppCompatActivity {
-
     private Game g;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
-        g.getinstance();
+        g=Game.createGame();
         PopulateRadioGroup();
         setBtn();
     }
@@ -27,19 +35,16 @@ public class configActivity extends AppCompatActivity {
         RadioGroup boardSizeGroup = (RadioGroup) findViewById(R.id.boardSizeRadioGroup);
         String[] sizes = getResources().getStringArray(R.array.boardSizes);
         for(int i =0; i < sizes.length; i++) {
-            if(i==0) {
-                RadioButton radioBtn = new RadioButton(this);
-                radioBtn.setText(sizes[i]);
-                radioBtn.setOnClickListener(new View.OnClickListener() {
+            RadioButton radioBtn = new RadioButton(this);
+            radioBtn.setText(sizes[i]);
+            radioBtn.setOnClickListener(new View.OnClickListener() {
 
-                    @Override
-                    public void onClick(View view) {
-                        // TODO: change Game Model's board size config ( NUM_COL, NUM_ROW)
-
-                    }
-                });
-                boardSizeGroup.addView(radioBtn);
-            }
+                @Override
+                public void onClick(View view) {
+                    // TODO: change Game Model's board size config ( NUM_COL, NUM_ROW)
+                }
+            });
+            boardSizeGroup.addView(radioBtn);
         }
         RadioGroup mineNumGroup = (RadioGroup) findViewById(R.id.mineNumRadioGroup);
         int[] nums = getResources().getIntArray(R.array.mineNums);
